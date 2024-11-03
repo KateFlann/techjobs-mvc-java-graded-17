73ce134ff08fc8c9ca88a3f9f020eef9e1473cb2
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-11-02 21:15:41 -0400

    GA3: Working on Tasks 2 and 3 - stopping point

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -26,29 +26,29 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-    @PostMapping(value="search/results")
-    public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+//    @PostMapping(value="search/results")
+//    public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
 //    If the user enters “all” in the search box, or if they leave the box empty, call the findAll() method from JobData.
-        if (searchTerm.toLowerCase()="all") {
-            return JobData.findAll();
-        } else if (searchTerm="") {
-            return JobData.findAll();
-        }
-//        Otherwise, send the search information to findByColumnAndValue.
-
-
-        //    In either case, store the results in a jobs ArrayList.
-        ArrayList<String, String> somethingSomething = new ArrayList<>();
+//        if (searchTerm.toLowerCase()="all") {
+//            return JobData.findAll();
+//        } else if (searchTerm="") {
+//            return JobData.findAll();
+//        }
+////        Otherwise, send the search information to findByColumnAndValue.
+//
+//
+//        //    In either case, store the results in a jobs ArrayList.
+//        ArrayList<String, String> somethingSomething = new ArrayList<>();
 
 
 //                  Pass jobs into the search.html view via the model parameter.
 //                  Pass ListController.columnChoices into the view, as the existing search handler does.
 
 
-        return results;
+//        return results;
 
-    }
+//    }
 
 }
 
