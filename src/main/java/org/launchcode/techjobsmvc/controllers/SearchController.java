7bdcad8466d323eb5e1f6aa883ae79d3ef798cc5
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-11-02 21:14:25 -0400

    GA3: Working on Tasks 2 and 3 - stopping point

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,12 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +25,30 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    @PostMapping(value="search/results")
+    public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+//    If the user enters “all” in the search box, or if they leave the box empty, call the findAll() method from JobData.
+        if (searchTerm.toLowerCase()="all") {
+            return JobData.findAll();
+        } else if (searchTerm="") {
+            return JobData.findAll();
+        }
+//        Otherwise, send the search information to findByColumnAndValue.
+
+
+        //    In either case, store the results in a jobs ArrayList.
+        ArrayList<String, String> somethingSomething = new ArrayList<>();
+
+
+//                  Pass jobs into the search.html view via the model parameter.
+//                  Pass ListController.columnChoices into the view, as the existing search handler does.
+
+
+        return results;
+
+    }
 
 }
 
